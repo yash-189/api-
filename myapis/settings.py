54fe042a71aa417dbf450e87e49de5d46eb3ae74
yash189 <yashsaini189@gmail.com>
2022-12-14 22:55:05 +0530

    commit

--- myapis/settings.py
+++ myapis/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'djongo.models.BigAutoField'
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
